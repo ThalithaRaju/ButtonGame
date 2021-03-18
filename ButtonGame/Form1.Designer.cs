9666871309d483896b74b286f12b302379853a79
ThalithaRaju <thalitharaju22@gmail.com>
2021-03-18 20:16:17 +0530

    Firstcommit

---
+++ ButtonGame/Form1.Designer.cs
@@ -0,0 +1,323 @@
+﻿namespace ButtonGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblWikets = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lbl = new System.Windows.Forms.Label();
+            this.btnOne = new System.Windows.Forms.Button();
+            this.btnTwo = new System.Windows.Forms.Button();
+            this.btnThree = new System.Windows.Forms.Button();
+            this.btnFour = new System.Windows.Forms.Button();
+            this.btnFive = new System.Windows.Forms.Button();
+            this.btnSix = new System.Windows.Forms.Button();
+            this.lblUser = new System.Windows.Forms.Label();
+            this.llRndom = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(579, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "X";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.lblTime);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.lblWikets);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.lbl);
+            this.panel1.Location = new System.Drawing.Point(1, 228);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(596, 32);
+            this.panel1.TabIndex = 1;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // lblTime
+            // 
+            this.lblTime.Location = new System.Drawing.Point(461, 3);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(133, 22);
+            this.lblTime.TabIndex = 14;
+            this.lblTime.Text = "00:00:00";
+            this.lblTime.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblTime.Click += new System.EventHandler(this.lblTime_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-2, -2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(33, 48);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 15;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(80, 2);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(15, 24);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "/";
+            // 
+            // lblWikets
+            // 
+            this.lblWikets.AutoSize = true;
+            this.lblWikets.Location = new System.Drawing.Point(62, 8);
+            this.lblWikets.Name = "lblWikets";
+            this.lblWikets.Size = new System.Drawing.Size(13, 13);
+            this.lblWikets.TabIndex = 2;
+            this.lblWikets.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(92, 8);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(13, 13);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "3";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // lbl
+            // 
+            this.lbl.AutoSize = true;
+            this.lbl.Location = new System.Drawing.Point(36, 10);
+            this.lbl.Name = "lbl";
+            this.lbl.Size = new System.Drawing.Size(25, 13);
+            this.lbl.TabIndex = 0;
+            this.lbl.Text = "WK";
+            // 
+            // btnOne
+            // 
+            this.btnOne.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOne.Location = new System.Drawing.Point(29, 119);
+            this.btnOne.Name = "btnOne";
+            this.btnOne.Size = new System.Drawing.Size(34, 35);
+            this.btnOne.TabIndex = 2;
+            this.btnOne.Text = "1";
+            this.btnOne.UseVisualStyleBackColor = true;
+            this.btnOne.Click += new System.EventHandler(this.btnOne_Click);
+            // 
+            // btnTwo
+            // 
+            this.btnTwo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTwo.Location = new System.Drawing.Point(125, 119);
+            this.btnTwo.Name = "btnTwo";
+            this.btnTwo.Size = new System.Drawing.Size(35, 35);
+            this.btnTwo.TabIndex = 3;
+            this.btnTwo.Text = "2";
+            this.btnTwo.UseVisualStyleBackColor = true;
+            this.btnTwo.Click += new System.EventHandler(this.btnTwo_Click);
+            // 
+            // btnThree
+            // 
+            this.btnThree.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThree.Location = new System.Drawing.Point(221, 119);
+            this.btnThree.Name = "btnThree";
+            this.btnThree.Size = new System.Drawing.Size(35, 35);
+            this.btnThree.TabIndex = 4;
+            this.btnThree.Text = "3";
+            this.btnThree.UseVisualStyleBackColor = true;
+            this.btnThree.Click += new System.EventHandler(this.btnThree_Click);
+            // 
+            // btnFour
+            // 
+            this.btnFour.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFour.Location = new System.Drawing.Point(317, 119);
+            this.btnFour.Name = "btnFour";
+            this.btnFour.Size = new System.Drawing.Size(34, 35);
+            this.btnFour.TabIndex = 5;
+            this.btnFour.Text = "4";
+            this.btnFour.UseVisualStyleBackColor = true;
+            this.btnFour.Click += new System.EventHandler(this.btnFour_Click);
+            // 
+            // btnFive
+            // 
+            this.btnFive.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFive.Location = new System.Drawing.Point(413, 119);
+            this.btnFive.Name = "btnFive";
+            this.btnFive.Size = new System.Drawing.Size(36, 35);
+            this.btnFive.TabIndex = 6;
+            this.btnFive.Text = "5";
+            this.btnFive.UseVisualStyleBackColor = true;
+            this.btnFive.Click += new System.EventHandler(this.btnFive_Click);
+            // 
+            // btnSix
+            // 
+            this.btnSix.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSix.Location = new System.Drawing.Point(509, 119);
+            this.btnSix.Name = "btnSix";
+            this.btnSix.Size = new System.Drawing.Size(33, 35);
+            this.btnSix.TabIndex = 7;
+            this.btnSix.Text = "6";
+            this.btnSix.UseVisualStyleBackColor = true;
+            this.btnSix.Click += new System.EventHandler(this.btnSix_Click);
+            // 
+            // lblUser
+            // 
+            this.lblUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUser.Location = new System.Drawing.Point(253, 31);
+            this.lblUser.Name = "lblUser";
+            this.lblUser.Size = new System.Drawing.Size(19, 23);
+            this.lblUser.TabIndex = 8;
+            this.lblUser.Text = "0";
+            // 
+            // llRndom
+            // 
+            this.llRndom.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.llRndom.Location = new System.Drawing.Point(290, 31);
+            this.llRndom.Name = "llRndom";
+            this.llRndom.Size = new System.Drawing.Size(23, 32);
+            this.llRndom.TabIndex = 9;
+            this.llRndom.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(269, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 37);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "/";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Minimum Score :50";
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Location = new System.Drawing.Point(96, 36);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(13, 13);
+            this.lblScore.TabIndex = 12;
+            this.lblScore.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(13, 36);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(84, 23);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "YourScore      :";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(598, 260);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.llRndom);
+            this.Controls.Add(this.lblUser);
+            this.Controls.Add(this.btnSix);
+            this.Controls.Add(this.btnFive);
+            this.Controls.Add(this.btnFour);
+            this.Controls.Add(this.btnThree);
+            this.Controls.Add(this.btnTwo);
+            this.Controls.Add(this.btnOne);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnOne;
+        private System.Windows.Forms.Button btnTwo;
+        private System.Windows.Forms.Button btnThree;
+        private System.Windows.Forms.Button btnFour;
+        private System.Windows.Forms.Button btnFive;
+        private System.Windows.Forms.Button btnSix;
+        private System.Windows.Forms.Label lblUser;
+        private System.Windows.Forms.Label llRndom;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblWikets;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lbl;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ ButtonGame/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ButtonGame
+{
+    
+    public partial class Form1 : Form
+    {
+     int  score = 50;
+       // int yourscore = 0;
+
+        int lifes = 3;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        System.Windows.Forms.Timer tmr = null;
+        private void StartTimer()
+        {
+            timer1 = new System.Windows.Forms.Timer();
+           timer1.Interval = 1000;
+            timer1.Tick += new EventHandler(timer1_Tick);
+            timer1.Enabled = true;
+        }
+
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.BackColor = Color.FromArgb(48, 119, 137);
+            panel1.BackColor = Color.White;
+            
+            pictureBox1.BackColor= Color.FromArgb(48, 119, 137);
+           
+
+           
+                                       
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void btnOne_Click(object sender, EventArgs e)
+        {
+            
+
+
+            lblUser.Text = btnOne.Text;
+            Random rnd = new Random();
+            int random = rnd.Next(1, 6);
+            llRndom.Text = random.ToString();
+            score = 50 + Convert.ToInt32(btnOne.Text);
+            lblScore.Text = score.ToString();
+            //if (lblUser.Text == llRndom.Text)
+            //{
+            //    lblWikets.Text = (lifes - 1).ToString();
+            //}
+        }
+
+        private void btnTwo_Click(object sender, EventArgs e)
+        {
+            lblUser.Text = btnTwo.Text;
+            Random rnd = new Random();
+            int random = rnd.Next(1, 6);
+            llRndom.Text = random.ToString();
+            score = 50 + Convert.ToInt32(btnTwo.Text);
+            lblScore.Text = score.ToString();
+        }
+
+        private void btnThree_Click(object sender, EventArgs e)
+        {
+            lblUser.Text = btnThree.Text;
+            Random rnd = new Random();
+            int random = rnd.Next(1, 6);
+            llRndom.Text = random.ToString();
+        }
+
+        private void btnFour_Click(object sender, EventArgs e)
+        {
+            lblUser.Text = btnFour.Text;
+            Random rnd = new Random();
+            int random = rnd.Next(1, 6);
+            llRndom.Text = random.ToString();
+        }
+
+        private void btnFive_Click(object sender, EventArgs e)
+        {
+            lblUser.Text = btnFive.Text;
+            Random rnd = new Random();
+            int random = rnd.Next(1, 6);
+            llRndom.Text = random.ToString();
+        }
+
+        private void btnSix_Click(object sender, EventArgs e)
+        {
+            lblUser.Text = btnSix.Text;
+            Random rnd = new Random();
+            int random = rnd.Next(1, 6);
+            llRndom.Text = random.ToString();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblTime_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lblTime.Text = DateTime.Now.ToString();
+        }
+    }
+}
